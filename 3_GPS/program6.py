cbf34834c80424021acaa4518a071fd6eaa1c5bc
Takehiko Shimojima <shimojima@gmail.com>
2018-10-13 08:13:57 +0900

    測位に利用した衛星の色を変えて表示

--- 3_GPS/program6.py
+++ 3_GPS/program6.py
@@ -5,12 +5,14 @@ from m5stack import lcd
 from math import radians, sin, cos
 
 gps_s = UART(2, tx=17, rx=16, baudrate=9600, timeout=200, buffer_size=256, lineend='\r\n')
+micropyGPS.MicropyGPS.supported_sentences.update({'GNGSA': micropyGPS.MicropyGPS.gpgsa})
 gps = micropyGPS.MicropyGPS(9, 'dd')
 
 def GPSwatch():
     n = 0
     tm_last = 0
     satellites = dict()
+    satellites_used = dict()
     while True:
         utime.sleep_ms(100)
         len = gps_s.any()
@@ -32,8 +34,9 @@ def GPSwatch():
                             lcd.print(str, 10, 0)
                             if gps.satellite_data_updated():
                                 putSatellites(satellites, gps.satellite_data, tm_now)
+                            putSatellitesUsed(satellites_used, gps.satellites_used, tm_now)
                             drawGrid()
-                            drawSatellites(satellites)
+                            drawSatellites(satellites, satellites_used)
                             if (n % 10) == 0:
                                 print("Mem free:", gc.mem_free())
                                 gc.collect()
@@ -46,6 +49,15 @@ def putSatellites(sats, new_sats, tm):
             print('pop(%s)' % str(k))
             sats.pop(k)
 
+def putSatellitesUsed(sats_used, sats, tm):
+    for x in sats:
+        sats_used.update({x: tm})
+    for k, v in sats_used.items():
+        if tm - v > 300:
+            print('pop_used(%s)' % str(k))
+            sats_used.pop(k)
+    print(sats_used)
+
 def drawGrid():
     for x in range(40, 121, 40):
         lcd.circle(160, 120, x, lcd.DARKGREY)
@@ -56,7 +68,7 @@ def drawGrid():
     for x in (('90', 155, 108), ('60', 195, 108), ('30', 235, 108), ('0', 275, 108)):
         lcd.print(x[0], x[1], x[2])
 
-def drawSatellites(sats):
+def drawSatellites(sats, sats_used):
     for k, v in sats.items():
         print(k, v[0])
         if v[0][0] != None and v[0][1] != None:
@@ -64,7 +76,8 @@ def drawSatellites(sats):
             lcd.lineByAngle(160, 120, 0, l, v[0][1])
             x = 160 + sin(radians(v[0][1])) * l
             y = 120 - cos(radians(v[0][1])) * l
-            lcd.circle(int(x), int(y), 4, lcd.GREEN, lcd.GREEN)
+            color = lcd.GREEN if k in sats_used else lcd.RED
+            lcd.circle(int(x), int(y), 4, color, color)
             lcd.print(str(k), int(x) + 9, int(y) - 7)
 
 testth=_thread.start_new_thread("GPS", GPSwatch, ())
